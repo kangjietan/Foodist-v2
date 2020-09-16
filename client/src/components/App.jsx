e3d30c124781fdea9fa4af4d4aada7cc7fe9907e
kangjietan <kangjietan@gmail.com>
2020-09-15 23:57:51 -0700

    Render different components/pages depending on url

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Search from './pages/search/Search';
+import List from './pages/list/List';
+import Random from './pages/random/Random';
+import Login from './pages/login/Login';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/:user/list" component={List} />
+            <Route exact path="/random" component={Random} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
