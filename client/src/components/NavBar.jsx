5a7ba69ba4da44ac8d8e3b801552fb4b8e21505a
kangjietan <kangjietan@gmail.com>
2020-09-16 20:31:49 -0700

    Navigation bar component and css variables

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+// Component variables
+const transitionSpeed = '600ms';
+const textPrimary = '#b6b6b6';
+const textSecondary = '#ececec';
+const bgPrimary = '#23232e';
+const bgSecondary = '#141418';
+
+const NavigationBar = styled.nav`
+`;
+
+function NavBar() {
+  return (
+    <NavigationBar>
+      
+    </NavigationBar>
+  );
+}
+
+export default NavBar;
