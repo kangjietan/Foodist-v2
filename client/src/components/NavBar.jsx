d5ff91306b63ea3724a86dca1d0125e0a541041e
kangjietan <kangjietan@gmail.com>
2020-09-16 20:48:25 -0700

    Html component styles

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -9,28 +9,115 @@ const textSecondary = '#ececec';
 const bgPrimary = '#23232e';
 const bgSecondary = '#141418';
 
-const NavigationBar = styled.nav`
-
-`;
-
-const NavigationBarNav = styled.ul`
-
-`;
-
-const LinkText = styled.span`
-
-`;
-
-const NavigationLink = styled.a`
-
-`;
-
-const Logo = styled.li`
-
-`;
-
+// .nav-item
 const NavigationItem = styled.li`
+  width: 100%;
+`;
 
+// .nav-link
+const NavigationLink = styled.a`
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  color: ${textPrimary};
+  text-decoration: none;
+  filter: grayscale(100%) opacity(0.7);
+  transition: ${transitionSpeed};
+
+  ${NavigationLink}:hover {
+    filter: grayscale(0%) opacity(1);
+    background: ${bgSecondary};
+    color: ${textSecondary};
+  }
+
+  ${NavigationLink} svg {
+    min-width: 2rem;
+    margin: 0 1.5rem;
+  }
+`;
+
+// .link-text
+const LinkText = styled.span`
+  display: none;
+  margin-left: 1rem;
+`;
+
+// .logo
+const Logo = styled.li`
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: ${textSecondary};
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  letter-spacing: 0.3ch;
+  width: 100%;
+
+  ${Logo} svg {
+    transform: rotate(0deg);
+    transition: transform ${transitionSpeed};
+  }
+`;
+
+// .navbar-nav
+const NavigationBarNav = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+`;
+
+// .navbar
+const NavigationBar = styled.nav`
+  position: fixed;
+  background-color: ${bgPrimary};
+  transition: width 200ms ease;
+
+  ${NavigationBar}:hover #logo svg {
+    transform: rotate(180deg);
+  }
+
+  @media only screen and (max-width: 600px) {
+    ${NavigationBar} {
+      bottom: 0;
+      width: 100vw;
+      height: 5rem;
+    }
+  
+    #logo {
+      display: none;
+    }
+  
+    ${NavigationBarNav} {
+      flex-direction: row;
+    }
+  
+    ${NavigationLink} {
+      justify-content: center;
+    }
+  }
+
+  @media only screen and (min-width: 600px) {
+    ${NavigationBar} {
+      top: 0;
+      width: 5rem;
+      height: 100vh;
+    }
+  
+    ${NavigationBar}:hover {
+      width: 16rem;
+      z-index: 10;
+    }
+
+    ${NavigationBar}:hover ${LinkText} {
+      display: inline;
+      transition: opacity ${transitionSpeed};
+    }
+  }
 `;
 
 function NavBar() {
