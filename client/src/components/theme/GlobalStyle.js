aee6bb2afa42b8b43fe9ef3dbfb06f2d5a2bde4a
kangjietan <kangjietan@gmail.com>
2020-09-19 11:43:22 -0700

    Import Montserrat font

--- client/src/components/theme/GlobalStyle.js
+++ client/src/components/theme/GlobalStyle.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;800&display=swap');
+
   root {
     font-size: 16px;
     font-family: "Open Sans";
