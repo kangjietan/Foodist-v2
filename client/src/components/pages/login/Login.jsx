5eae1f681980787e290e91549ba67c594d2b0850
kangjietan <kangjietan@gmail.com>
2020-09-19 11:37:52 -0700

    Add form state and handlers

--- client/src/components/pages/login/Login.jsx
+++ client/src/components/pages/login/Login.jsx
@@ -11,11 +11,47 @@ class Login extends Component {
     super(props);
 
     this.state = {
-
+      loginUsername: '',
+      loginPassword: '',
+      registerUsername: '',
+      registerPassword: '',
+      registerPassword2: '',
+      rightPanelActive: false,
     };
+
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleFormSubmission = this.handleFormSubmission.bind(this);
+    this.enableRightPanelActive = this.enableRightPanelActive.bind(this);
+    this.disableRightPanelActive = this.disableRightPanelActive.bind(this);
+  }
+
+  handleFormChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleFormSubmission(event) {
+    event.preventDefault();
+  }
+
+  enableRightPanelActive() {
+    this.setState({ rightPanelActive: true });
+  }
+
+  disableRightPanelActive() {
+    this.setState({ rightPanelActive: false });
   }
 
   render() {
+    const {
+      loginUsername,
+      loginPassword,
+      registerUsername,
+      registerPassword,
+      registerPassword2,
+      rightPanelActive
+    } = this.state;
+    
     return (
       <FormBodyWrapper>
         Login
