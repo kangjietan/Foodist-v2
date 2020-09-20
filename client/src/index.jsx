8a822070d2bb0a1ca722350742eb2da7c506d9fa
kangjietan <kangjietan@gmail.com>
2020-09-19 19:14:01 -0700

    Wrap App in store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-import App from './components/App.jsx';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
