4b0808facc145a913cfd8cd104a63a9b8629e1a4
kangjietan <kangjietan@gmail.com>
2020-09-23 22:24:52 -0700

    Add if redux dev tools exist in development env.

--- client/src/store.js
+++ client/src/store.js
@@ -6,15 +6,27 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 
+const enhancers = [];
+
 const middleware = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers,
+);
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composedEnhancers,
 );
 
 export default store;
