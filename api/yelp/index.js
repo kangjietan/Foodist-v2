00a9fbc63fc943aa84275b8405eb56199c40dd17
kangjietan <kangjietan@gmail.com>
2020-09-26 21:16:43 -0700

    Yelp API business search helper

---
+++ api/yelp/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const axios = require("axios");
+
+const { YELP_API_KEY } = process.env;
+
+const businessSearch = (params) => {
+  const url = "https://api.yelp.com/v3/businesses/search";
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: YELP_API_KEY,
+    },
+    params,
+    url: `${url}`,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(options)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+module.exports = {
+  businessSearch,
+};
