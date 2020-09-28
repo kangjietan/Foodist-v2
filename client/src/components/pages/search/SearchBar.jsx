059947b61199599beadc841c0e032e3a54322b5c
kangjietan <kangjietan@gmail.com>
2020-09-27 23:38:23 -0700

    Keep text above valid input. Handle form change.

--- client/src/components/pages/search/SearchBar.jsx
+++ client/src/components/pages/search/SearchBar.jsx
@@ -4,7 +4,8 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   font-family: 'Roboto', sans-serif;
-  // width: 100%;
+  max-width: 1220px;
+  margin-top: 2rem;
 `;
 
 const FormContainer = styled.div`
@@ -13,13 +14,12 @@ const FormContainer = styled.div`
 
 const FormInputContainer = styled.div`
   position: relative;
-  height: 50px;
+  height: 75px;
   overflow: hidden;
 
   // background: #fff;
   // box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   width: 100%;
-  margin-bottom: 2rem;
   color: #b19cd9;
 `;
 
@@ -44,7 +44,7 @@ const FormLabel = styled.label`
   width: 100%;
   height: 100%;
   pointer-events: none;
-  // border-bottom: 1px solid black;
+  border-bottom: 1px solid black;
 
   &::after {
     content: "";
@@ -61,7 +61,7 @@ const FormLabel = styled.label`
 
 const FormLabelText = styled.span`
   position: absolute;
-  bottom: 5px;
+  bottom: 6px;
   left: 0;
   transition: all 0.3s ease;
 `;
@@ -73,12 +73,15 @@ class SearchBar extends Component {
     this.state = {
       searchTermActive: false,
       locationActive: false,
+      searchInput: "",
+      locationInput: "",
     };
 
     this.enableSearchTermActive = this.enableSearchTermActive.bind(this);
     this.disableSearchTermActive = this.disableSearchTermActive.bind(this);
     this.enableLocationActive = this.enableLocationActive.bind(this);
     this.disableLocationActive = this.disableLocationActive.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   }
 
   enableSearchTermActive() {
@@ -97,37 +100,56 @@ class SearchBar extends Component {
     this.setState({ locationActive: false });
   }
 
+  handleFormChange(event) {
+    const { target } = event;
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  }
+
   render() {
-    const { searchTermActive, locationActive } = this.state;
+    const { searchTermActive, locationActive, searchInput, locationInput } = this.state;
     let searchTermStyle = {}, locationStyle = {};
 
-    if (searchTermActive) {
-      searchTermStyle = {
-        transform: "translateY(-150%)",
-        fontSize: "14px",
-        color: "#5fa8d3",
-      }
-    }
+    let labelFocusStyle = {
+      transform: "translateY(-150%)",
+      fontSize: "14px",
+      color: "#5fa8d3",
+    };
 
-    if (locationActive) {
-      locationStyle = {
-        transform: "translateY(-150%)",
-        fontSize: "14px",
-        color: "#5fa8d3",
-      }
-    }
+    let labelFilledStyle = {
+      transform: "translateY(-150%)",
+    };
+
+    if (searchTermActive) searchTermStyle = labelFocusStyle;
+  
+    if (locationActive)  locationStyle = labelFocusStyle;
+
+    if (!searchTermActive && searchInput.length !== 0) searchTermStyle = labelFilledStyle;
+
+    if (!locationActive && locationInput.length !== 0) locationStyle = labelFilledStyle;
 
     return (
       <Container>
         <FormContainer>
           <FormInputContainer>
-            <FormInput onFocus={this.enableSearchTermActive} onBlur={this.disableSearchTermActive} />
+            <FormInput
+              name="searchInput"
+              onFocus={this.enableSearchTermActive}
+              onBlur={this.disableSearchTermActive}
+              value={searchInput}
+              onChange={this.handleFormChange} />
             <FormLabel>
               <FormLabelText style={searchTermStyle}>Term</FormLabelText>
             </FormLabel>
           </FormInputContainer>
           <FormInputContainer>
-            <FormInput onFocus={this.enableLocationActive} onBlur={this.disableLocationActive} />
+            <FormInput
+              name="locationInput"
+              onFocus={this.enableLocationActive}
+              onBlur={this.disableLocationActive}
+              value={locationInput}
+              onChange={this.handleFormChange} />
             <FormLabel>
               <FormLabelText style={locationStyle}>Location</FormLabelText>
             </FormLabel>
