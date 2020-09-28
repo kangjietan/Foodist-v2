336494d2352cf992f3e7a25d4674c2675614be8c
kangjietan <kangjietan@gmail.com>
2020-09-28 00:24:57 -0700

    Increase font size and add search button.

--- client/src/components/pages/search/SearchBar.jsx
+++ client/src/components/pages/search/SearchBar.jsx
@@ -4,23 +4,30 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   font-family: 'Roboto', sans-serif;
-  max-width: 1220px;
-  margin-top: 2rem;
+  margin-top: 1rem;
+  width: 75%;
+  max-width: 1200px;
+  font-size: 1.25rem;
 `;
 
 const FormContainer = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 1200px;
+  background: #fff;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 `;
 
 const FormInputContainer = styled.div`
   position: relative;
   height: 75px;
   overflow: hidden;
-
-  // background: #fff;
-  // box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   width: 100%;
   color: #b19cd9;
+  &:first-child {
+    margin-right: 1rem;
+  }
 `;
 
 const FormInput = styled.input`
@@ -30,6 +37,7 @@ const FormInput = styled.input`
   padding-top: 20px;
   border: none;
   outline: none;
+  font-size: 1rem;
 
   &:focus + ${FormLabel}::after,
   &:active + ${FormLabel}::after {
@@ -44,7 +52,7 @@ const FormLabel = styled.label`
   width: 100%;
   height: 100%;
   pointer-events: none;
-  border-bottom: 1px solid black;
+  // border-bottom: 1px solid black;
 
   &::after {
     content: "";
@@ -64,6 +72,20 @@ const FormLabelText = styled.span`
   bottom: 6px;
   left: 0;
   transition: all 0.3s ease;
+  padding: 0.10rem 0.25rem;
+`;
+
+const SearchButton = styled.button`
+  // border: none;
+  outline: none;
+`;
+
+const ButtonContainer = styled.div`
+  padding: 0.5rem 0.5rem;
+  margin: auto;
+  & svg {
+    width: 3rem;
+  }
 `;
 
 class SearchBar extends Component {
@@ -113,7 +135,7 @@ class SearchBar extends Component {
 
     let labelFocusStyle = {
       transform: "translateY(-150%)",
-      fontSize: "14px",
+      fontSize: "0.9rem",
       color: "#5fa8d3",
     };
 
@@ -122,8 +144,8 @@ class SearchBar extends Component {
     };
 
     if (searchTermActive) searchTermStyle = labelFocusStyle;
-  
-    if (locationActive)  locationStyle = labelFocusStyle;
+
+    if (locationActive) locationStyle = labelFocusStyle;
 
     if (!searchTermActive && searchInput.length !== 0) searchTermStyle = labelFilledStyle;
 
@@ -134,26 +156,56 @@ class SearchBar extends Component {
         <FormContainer>
           <FormInputContainer>
             <FormInput
+              type="text"
               name="searchInput"
               onFocus={this.enableSearchTermActive}
               onBlur={this.disableSearchTermActive}
               value={searchInput}
-              onChange={this.handleFormChange} />
+              onChange={this.handleFormChange}
+            />
             <FormLabel>
               <FormLabelText style={searchTermStyle}>Term</FormLabelText>
             </FormLabel>
           </FormInputContainer>
           <FormInputContainer>
             <FormInput
+              type="text"
               name="locationInput"
               onFocus={this.enableLocationActive}
               onBlur={this.disableLocationActive}
               value={locationInput}
-              onChange={this.handleFormChange} />
+              onChange={this.handleFormChange}
+            />
             <FormLabel>
               <FormLabelText style={locationStyle}>Location</FormLabelText>
             </FormLabel>
           </FormInputContainer>
+          {/* <SearchButton>Search</SearchButton> */}
+          <ButtonContainer>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="search"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className=""
+            >
+              <g className="fa-group">
+                <path
+                  fill="currentColor"
+                  d="M208 80a128 128 0 1 1-90.51 37.49A127.15 127.15 0 0 1 208 80m0-80C93.12 0 0 93.12 0 208s93.12 208 208 208 208-93.12 208-208S322.88 0 208 0z"
+                  className=""
+                ></path>
+                <path
+                  fill="currentColor"
+                  d="M504.9 476.7L476.6 505a23.9 23.9 0 0 1-33.9 0L343 405.3a24 24 0 0 1-7-17V372l36-36h16.3a24 24 0 0 1 17 7l99.7 99.7a24.11 24.11 0 0 1-.1 34z"
+                  className=""
+                ></path>
+              </g>
+            </svg>
+          </ButtonContainer>
         </FormContainer>
       </Container>
     );
