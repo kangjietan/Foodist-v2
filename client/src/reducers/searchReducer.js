c51da79054ad3e843228119fc42eaba02a2b244f
kangjietan <kangjietan@gmail.com>
2020-09-28 15:16:01 -0700

    Add search reducer.

---
+++ client/src/reducers/searchReducer.js
@@ -0,0 +1,17 @@
+import * as actions from "../actions/types";
+
+let initialState = {
+  searchResults: {},
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case actions.SEARCH_BUSINESSES_YELP:
+      return {
+        ...state,
+        searchResults: Object.assign(state.searchResults, {
+          [action.offset]: action.payload,
+        }),
+      };
+  }
+}
