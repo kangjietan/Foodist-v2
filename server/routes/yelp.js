f10a4624b7eb2e81f394bc304a8855853e349300
kangjietan <kangjietan@gmail.com>
2020-09-28 17:50:21 -0700

    Add YELP API search route.

--- server/routes/yelp.js
+++ server/routes/yelp.js
@@ -2,4 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
+const yelp = require("../../api/yelp");
+
+router.get("/search", (req, res) => {
+  const params = req.query;
+  params.limit = 50;
+
+  yelp
+    .businessSearch(params)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
 module.exports = router;
