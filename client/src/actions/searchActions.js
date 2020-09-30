cb559215df12bfa14fae32a89f9d539b1c6b55d9
kangjietan <kangjietan@gmail.com>
2020-09-29 19:04:08 -0700

    Return new promise.

--- client/src/actions/searchActions.js
+++ client/src/actions/searchActions.js
@@ -3,20 +3,25 @@ import * as actions from "../actions/types";
 import axios from "axios";
 
 export const searchBusinessesYelp = (params) => (dispatch) => {
-  axios
-    .get("/yelp/search", { params })
-    .then((response) => {
-      console.log(response);
-      let data = {};
-      response.data.businesses.forEach((entry) => data[entry.id] = entry);
+  return new Promise((resolve, reject) => {
+    axios
+      .get("/yelp/search", { params })
+      .then((response) => {
+        console.log(response);
+        let data = {};
+        response.data.businesses.forEach((entry) => (data[entry.id] = entry));
 
-      dispatch({
-        type: actions.SEARCH_BUSINESSES_YELP,
-        payload: data,
-        offset: params.offset,
+        dispatch({
+          type: actions.SEARCH_BUSINESSES_YELP,
+          payload: data,
+          offset: params.offset,
+        });
+
+        resolve("Stored");
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
       });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  });
 };
