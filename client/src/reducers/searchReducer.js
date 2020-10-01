1e24dbde07757ee4ea95ba15106307a681b7a0ee
kangjietan <kangjietan@gmail.com>
2020-09-30 19:25:29 -0700

    Add term and location to state. Add update search term and location case.

--- client/src/reducers/searchReducer.js
+++ client/src/reducers/searchReducer.js
@@ -2,6 +2,8 @@ import * as actions from "../actions/types";
 
 let initialState = {
   searchResults: {},
+  term: "",
+  location: "",
 };
 
 export default function (state = initialState, action) {
@@ -12,6 +14,13 @@ export default function (state = initialState, action) {
         searchResults: Object.assign(state.searchResults, {
           [action.offset]: action.payload,
         }),
+      };
+
+    case actions.UPDATE_SEARCH_TERM_AND_LOCATION:
+      return {
+        ...state,
+        term: action.payload.term,
+        location: action.payload.location,
       };
 
     default:
