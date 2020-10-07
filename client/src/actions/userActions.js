9e3fc70ba7ad93bb25a884d330b4e4f21cd9e827
kangjietan <kangjietan@gmail.com>
2020-10-06 20:52:04 -0700

    Add/remove custom/favorites list actions.

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -6,3 +6,31 @@ export const setUserIsLoggedIn = (status) => (dispatch) => {
     payload: status,
   });
 };
+
+export const addToCustomList = (business) => (dispatch) => {
+  dispatch({
+    type: actions.ADD_TO_CUSTOM_LIST,
+    payload: business,
+  });
+};
+
+export const removeFromCustomList = (business) => (dispatch) => {
+  dispatch({
+    type: actions.REMOVE_FROM_CUSTOM_LIST,
+    payload: { id: business.id },
+  });
+};
+
+export const addToFavoritesList = (business) => (dispatch) => {
+  dispatch({
+    type: actions.ADD_TO_FAVORITES_LIST,
+    payload: business,
+  });
+};
+
+export const removeFromFavoritesList = (business) => (dispatch) => {
+  dispatch({
+    type: actions.REMOVE_FROM_FAVORITES_LIST,
+    payload: { id: business.id },
+  });
+};
