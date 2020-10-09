4f0e65eb69fb08ec53315f33a18e6718637b1f2c
kangjietan <kangjietan@gmail.com>
2020-10-08 19:12:30 -0700

    Connect component to store. Map over list and render ListBusiness component.

--- client/src/components/pages/list/List.jsx
+++ client/src/components/pages/list/List.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
-function List() {
+import styled from 'styled-components';
+
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+
+import ListBusiness from '../../businesses/ListBusiness';
+
+const Container = styled.div`
+
+`;
+
+const ListContainer = styled.div`
+
+`;
+
+function List(props) {
+  let list = props.customList ? props.customList : {};
+  let businesses = Object.keys(list).map((id) => list[id]);
+
   return (
-    <div>List</div>
+    <Container>
+      <ListContainer>
+        {businesses.map((business) => <ListBusiness business={business} key={business.id} /> )}
+      </ListContainer>
+    </Container>
   );
 }
 
-export default List;
+List.propTypes = {};
+
+const mapStateToProps = (state) => ({
+  customList: state.user.customList,
+  favoritesList: state.user.favoritesList,
+});
+
+export default connect(mapStateToProps, null)(List);
