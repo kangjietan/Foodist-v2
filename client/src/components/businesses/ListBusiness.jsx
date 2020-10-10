823ece76e88ba53a7323eaf22fbaf0fe3a048a84
kangjietan <kangjietan@gmail.com>
2020-10-09 21:23:10 -0700

    Render business information.

--- client/src/components/businesses/ListBusiness.jsx
+++ client/src/components/businesses/ListBusiness.jsx
@@ -7,15 +7,103 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addToCustomList, removeFromCustomList, addToFavoritesList, removeFromFavoritesList } from '../../actions/userActions';
 
-const Container = styled.div`
+import { formatTopics, formatMethods, getRatingsUrl } from '../utils';
 
+const Container = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  padding: 2rem;
 `;
+
+const ImageContainer = styled.div`
+  min-width: 15rem;
+  max-width: 15rem;
+  height: 10rem;
+  margin-right: 1rem;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const MainInformationContainer = styled.div`
+  padding: 1rem;
+`;
+
+const BusinessName = styled.div`
+  font-size: 1.5rem;
+`;
+
+const RatingContainer = styled.div`
+  display: flex;
+`;
+
+const Rating = styled.div`
+  margin-right: 0.25rem;
+`;
+
+const AddressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ReviewCount = styled.div``;
+
+const Price = styled.div``;
+
+const CategoriesTransactionsContainer = styled.div``;
+
+const Categories = styled.div``;
+
+const Transactions = styled.div``;
 
 function ListBusiness(props) {
   const { business } = props;
   return (
     <Container>
-      {business.name}
+      <ImageContainer>
+        <Image src={business.image_url} />
+      </ImageContainer>
+      <MainInformationContainer>
+        <BusinessName>
+          {business.name}
+        </BusinessName>
+        <RatingContainer>
+          {
+            business.rating
+              ?
+              <Rating>
+                <img src={getRatingsUrl(business.rating)} />
+              </Rating>
+              :
+              null
+          }
+          <ReviewCount>{business.review_count}&nbsp;</ReviewCount>
+          {
+            business.price
+              ?
+              <Price><span>&#183;</span>&nbsp;{business.price}</Price>
+              :
+              null
+          }
+        </RatingContainer>
+        <AddressContainer>
+          <span>{business.location.display_address[0]}</span>
+          <span>{business.location.display_address[1]}</span>
+        </AddressContainer>
+        <CategoriesTransactionsContainer>
+          <Categories>{formatTopics(business.categories)}</Categories>
+          {
+            business.transactions.length !== 0
+              ?
+              <Transactions>{`Offers: ${formatMethods(business.transactions)}`}</Transactions>
+              :
+              null
+          }
+        </CategoriesTransactionsContainer>
+      </MainInformationContainer>
     </Container>
   );
 }
