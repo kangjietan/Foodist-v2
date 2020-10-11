7e5f37b9b5af84ebf6a3deae902a8828fe9c5a59
kangjietan <kangjietan@gmail.com>
2020-10-10 18:00:52 -0700

    Add dropdown button and list. Add yelp logo.

--- client/src/components/businesses/ListBusiness.jsx
+++ client/src/components/businesses/ListBusiness.jsx
@@ -12,13 +12,16 @@ import { formatTopics, formatMethods, getRatingsUrl } from '../utils';
 const Container = styled.div`
   display: flex;
   margin-bottom: 1rem;
-  padding: 2rem;
+  padding: 1.5rem;
+  font-size: 1.25rem;
+  position: relative;
+  border: 1px solid black;
 `;
 
 const ImageContainer = styled.div`
-  min-width: 15rem;
-  max-width: 15rem;
-  height: 10rem;
+  min-width: 14rem;
+  max-width: 14rem;
+  height: 14rem;
   margin-right: 1rem;
 `;
 
@@ -28,12 +31,16 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
-const MainInformationContainer = styled.div`
+const InformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   padding: 1rem;
+  gap: 0.3rem;
 `;
 
 const BusinessName = styled.div`
   font-size: 1.5rem;
+  font-weight: 700;
 `;
 
 const RatingContainer = styled.div`
@@ -59,6 +66,58 @@ const Categories = styled.div``;
 
 const Transactions = styled.div``;
 
+const ButtonContainer = styled.div`
+  position: absolute;
+  /* top: 3rem; */
+  top: 0.25rem;
+  right: 0.25rem;
+`;
+
+const DropDownButton = styled.button`
+  outline: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+  width: 1.1rem;
+`;
+
+const DropDownList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  position: absolute;
+  background: white;
+  margin-top: 10px;
+  padding: 0;
+  width: 225px;
+  height: 100px;
+  border: 1px solid black;
+  border-radius: 5px;
+  transition: all 0.4s ease;
+  transform: translateY(-10px);
+  opacity: 0;
+  visibility: hidden;
+  right: 0;
+
+  ${DropDownButton}:focus + & {
+    transform: translateY(0);
+    opacity: 1;
+    visibility: visible;
+  }
+`;
+
+const YelpLogo = styled.div`
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+
+  & img {
+    width: 50px;
+  }
+`;
+
 function ListBusiness(props) {
   const { business } = props;
   return (
@@ -66,7 +125,7 @@ function ListBusiness(props) {
       <ImageContainer>
         <Image src={business.image_url} />
       </ImageContainer>
-      <MainInformationContainer>
+      <InformationContainer>
         <BusinessName>
           {business.name}
         </BusinessName>
@@ -103,7 +162,32 @@ function ListBusiness(props) {
               null
           }
         </CategoriesTransactionsContainer>
-      </MainInformationContainer>
+        <ButtonContainer>
+          <DropDownButton>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="ellipsis-v"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 128 512"
+              className="">
+              <path fill="currentColor" d="M64 208c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM16 104c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48zm0 304c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48z"
+                className="">
+              </path>
+            </svg>
+          </DropDownButton>
+          <DropDownList>
+
+          </DropDownList>
+        </ButtonContainer>
+      </InformationContainer>
+      <YelpLogo>
+        <a href={business.url} target='_blank'>
+          <img src='./images/Yelp_Logo.svg' />
+        </a>
+      </YelpLogo>
     </Container>
   );
 }
