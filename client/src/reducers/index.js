0af5a68be92e2f2568878c3f5308b305111faeb9
kangjietan <kangjietan@gmail.com>
2020-10-12 18:30:35 -0700

    Bring in map reducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import userReducer from "./userReducer";
 import searchReducer from "./searchReducer";
+import mapReducer from "./mapReducer";
 
 export default combineReducers({
   user: userReducer,
   search: searchReducer,
+  map: mapReducer,
 });
