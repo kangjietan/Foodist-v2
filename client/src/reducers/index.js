131d4a300b0f3604d77c76d5ea8eda42512881d7
kangjietan <kangjietan@gmail.com>
2020-10-13 19:09:29 -0700

    Bring favorites lists reducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import searchReducer from "./searchReducer";
 import mapReducer from "./mapReducer";
+import favoriteListReducer from "./favoritesListReducer";
 
 export default combineReducers({
   user: userReducer,
   search: searchReducer,
   map: mapReducer,
+  favoriteList: favoriteListReducer,
 });
