1b8ce199659ce05b061f0c25f9cf5f229f70599e
kangjietan <kangjietan@gmail.com>
2020-10-13 19:10:07 -0700

    Add get business info and update action.

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,5 +1,7 @@
 import * as actions from "../actions/types";
 
+import axios from "axios";
+
 export const setUserIsLoggedIn = (status) => (dispatch) => {
   dispatch({
     type: actions.SET_USER_IS_LOGGED_IN,
@@ -32,5 +34,24 @@ export const removeFromFavoritesList = (business) => (dispatch) => {
   dispatch({
     type: actions.REMOVE_FROM_FAVORITES_LIST,
     payload: { id: business.id },
+  });
+};
+
+export const getBusinessInfoAndUpdate = (business) => (dispatch) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`/yelp/business/${business.id}`)
+      .then((response) => {
+        dispatch({
+          type: actions.GET_BUSINESS_INFO_AND_UPDATE,
+          payload: response.data,
+        });
+
+        resolve(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
   });
 };
