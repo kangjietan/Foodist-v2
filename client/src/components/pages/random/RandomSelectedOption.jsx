2334d43ea801f87a293e1dd2ff2bcf3fd7bbb85e
kangjietan <kangjietan@gmail.com>
2020-10-17 21:19:39 -0700

    Add component to render selected option.

---
+++ client/src/components/pages/random/RandomSelectedOption.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  margin-left: auto;
+`;
+
+function RandomSelectedOption() {
+  return (
+    <Container>
+      Selected option.
+    </Container>
+  );
+}
+
+export default RandomSelectedOption;
