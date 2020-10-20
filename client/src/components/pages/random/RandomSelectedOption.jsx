fb8c9a1286bbef9e6ba6e4488720622210e72c40
kangjietan <kangjietan@gmail.com>
2020-10-19 17:15:41 -0700

    Add resuable input styled component.

--- client/src/components/pages/random/RandomSelectedOption.jsx
+++ client/src/components/pages/random/RandomSelectedOption.jsx
@@ -22,22 +22,30 @@ const TermLocationInputContainer = styled.div`
   margin-right: 1rem;
 `;
 
-const TermInput = styled.input`
+const Input = styled.input`
   height: 40px;
   font-size: 1.1rem;
   padding: 0;
   border: 1px solid #2f3640;
   border-radius: 5px;
+  padding-left: 0.5rem;
+  transition: all 0.5s;
+
+  &:focus {
+    background: #2f3640;
+    color: white;
+  }
+
+  @media screen and (max-width: 700px) {
+    width: 175px;
+  }
+`;
+
+const TermInput = styled(Input)`
   margin-right: 0.2rem;
 `;
 
-const LocationInput = styled.input`
-  height: 40px;
-  font-size: 1.1rem;
-  padding: 0;
-  border: 1px solid #2f3640;
-  border-radius: 5px;
-`;
+const LocationInput = styled(Input)``;
 
 function RandomSelectedOption({ option }) {
   if (option === 'Term and Location') {
@@ -52,13 +60,13 @@ function RandomSelectedOption({ option }) {
     );
   } else if (option === 'Your Favorites') {
     return (
-      <SelectedContainer>
+      <SelectedContainer style={{ marginLeft: '1rem' }}>
         <RandomizeButton>Randomize</RandomizeButton>
       </SelectedContainer>
     );
   } else if (option === 'Custom List') {
     return (
-      <SelectedContainer>
+      <SelectedContainer style={{ marginLeft: '1rem' }}>
         <RandomizeButton>Randomize</RandomizeButton>
       </SelectedContainer>
     );
