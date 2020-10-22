523cd398d607abf5caa7991a32e10730a2ecb675
kangjietan <kangjietan@gmail.com>
2020-10-22 16:25:27 -0700

    Bring in random reducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import userReducer from "./userReducer";
 import searchReducer from "./searchReducer";
 import mapReducer from "./mapReducer";
 import favoritesListReducer from "./favoritesListReducer";
+import randomReducer from "./randomReducer";
 
 export default combineReducers({
   user: userReducer,
   search: searchReducer,
   map: mapReducer,
   favoritesList: favoritesListReducer,
+  random: randomReducer,
 });
