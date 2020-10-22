a4d09ff2c0a388531742c7476c8582acb7d4f334
kangjietan <kangjietan@gmail.com>
2020-10-22 16:25:40 -0700

    Add generate random int util.

--- client/src/components/utils/index.js
+++ client/src/components/utils/index.js
@@ -32,3 +32,10 @@ export function getRatingsUrl(num) {
 
   return url;
 }
+
+// Get random int for generating random business
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
