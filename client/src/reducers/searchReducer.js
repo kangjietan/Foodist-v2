14706e8ee064e0a2231aecb0b1af642644e28072
kangjietan <kangjietan@gmail.com>
2020-10-24 00:35:16 -0700

    Add initialize object to Object.assign.

--- client/src/reducers/searchReducer.js
+++ client/src/reducers/searchReducer.js
@@ -12,7 +12,7 @@ export default function (state = initialState, action) {
     case actions.SEARCH_BUSINESSES_YELP:
       return {
         ...state,
-        searchResults: Object.assign(state.searchResults, {
+        searchResults: Object.assign({}, state.searchResults, {
           [action.offset]: action.payload,
         }),
       };
