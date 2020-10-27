7fc89ee8dd2e8c4bfc2f2ed43d966564307034fe
kangjietan <kangjietan@gmail.com>
2020-10-26 18:44:32 -0700

    Connect component to store and get random business. Render random businesss component. Set up google maps embed.

--- client/src/components/pages/random/Random.jsx
+++ client/src/components/pages/random/Random.jsx
@@ -4,18 +4,70 @@ import styled from 'styled-components';
 
 import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
+import { updateRandomBusiness } from '../../../actions/randomActions';
+
 import RandomDropDown from './RandomDropDown';
 
-const Container = styled.div`
+import RandomBusiness from './RandomBusiness';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-function Random() {
+const RandomBusinessAndMapContainer = styled.div`
+  display: flex;
+`;
+
+const RandomBusinessContainer = styled.div``;
+
+const MapContainer = styled.div``;
+
+function Random(props) {
+  const { GOOGLE_MAPS_API_KEY } = process.env;
+
+  const { randomBusiness } = props;
+
+  let query;
+
+  if (Object.keys(randomBusiness).length !== 0) {
+    query = randomBusiness.name + randomBusiness.location.display_address;
+  }
+
   return (
     <Container>
+      <h1>Not sure where to eat? Have a random business chosen for you.</h1>
       <RandomDropDown />
+      {
+        Object.keys(randomBusiness).length > 1
+          ?
+          <RandomBusinessAndMapContainer>
+            <RandomBusinessContainer>
+              <RandomBusiness business={randomBusiness} />
+            </RandomBusinessContainer>
+            <MapContainer>
+              <iframe
+                frameBorder='0'
+                style={{ border: '0', width: '100%', height: '100%' }}
+                src={`https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAPS_API_KEY}&q=${query}`}>
+              </iframe>
+            </MapContainer>
+          </RandomBusinessAndMapContainer>
+          :
+          null
+      }
     </Container>
   );
 }
 
-export default Random;
+Random.propTypes = {};
+
+const mapStateToProps = (state) => ({
+  randomBusiness: state.random.randomBusiness,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Random);
