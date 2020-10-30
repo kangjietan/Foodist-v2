d4bc5934b259101779263f5af32cb6be12725e61
kangjietan <kangjietan@gmail.com>
2020-10-30 00:06:17 -0700

    Refactor to class component. Add current random list as prop. Update current random business state if previously empty or prop changes.

--- client/src/components/pages/random/Random.jsx
+++ client/src/components/pages/random/Random.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styled from 'styled-components';
 
@@ -9,63 +9,116 @@ import { updateRandomBusiness } from '../../../actions/randomActions';
 
 import RandomDropDown from './RandomDropDown';
 
-import RandomBusiness from './RandomBusiness';
+import RandomBusiness from '../../businesses/RandomBusiness';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 1rem;
 `;
 
 const RandomBusinessAndMapContainer = styled.div`
   display: flex;
+  width: 100%;
+  height: 75vh;
 `;
 
-const RandomBusinessContainer = styled.div``;
+const RandomBusinessContainer = styled.div`
+  flex: 1;
+  margin-right: 1rem;
+  border: 1px solid #eeeeef;
+  padding: 1rem 1rem;
+`;
 
-const MapContainer = styled.div``;
+const MapContainer = styled.div`
+  flex: 1.75;
+  border: 1px solid #eeeeef;
+  padding: 1rem 1rem;
+`;
 
-function Random(props) {
-  const { GOOGLE_MAPS_API_KEY } = process.env;
+class Random extends Component {
+  constructor(props) {
+    super(props);
 
-  const { randomBusiness } = props;
-
-  let query;
-
-  if (Object.keys(randomBusiness).length !== 0) {
-    query = randomBusiness.name + randomBusiness.location.display_address;
+    this.state = {
+      currentRandomBusiness: {},
+    }
   }
 
-  return (
-    <Container>
-      <h1>Not sure where to eat? Have a random business chosen for you.</h1>
-      <RandomDropDown />
-      {
-        Object.keys(randomBusiness).length > 1
-          ?
-          <RandomBusinessAndMapContainer>
-            <RandomBusinessContainer>
-              <RandomBusiness business={randomBusiness} />
-            </RandomBusinessContainer>
-            <MapContainer>
-              <iframe
-                frameBorder='0'
-                style={{ border: '0', width: '100%', height: '100%' }}
-                src={`https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAPS_API_KEY}&q=${query}`}>
-              </iframe>
-            </MapContainer>
-          </RandomBusinessAndMapContainer>
-          :
-          null
+  componentDidMount() {
+    this.updateCurrentRandomBusiness();
+  }
+
+  componentDidUpdate(prevProps) {
+    // console.log(prevProps, this.props);
+    const prevBusiness = prevProps.randomBusiness;
+    const business = this.props.randomBusiness;
+    if (Object.keys(prevBusiness).length === 0 && Object.keys(business).length > 1) {
+      this.updateCurrentRandomBusiness();
+    }
+
+    if (prevBusiness.id) {
+      if (prevBusiness.id !== business.id) {
+        this.updateCurrentRandomBusiness();
       }
-    </Container>
-  );
+    }
+  }
+
+  updateCurrentRandomBusiness() {
+    const { randomBusiness } = this.props;
+
+    if (Object.keys(randomBusiness).length > 1) {
+      this.setState({ currentRandomBusiness: randomBusiness });
+    }
+  }
+
+  clearCurrentRandomBusiness() {
+    this.setState({ currentRandomBusiness: {} });
+  }
+
+  render() {
+    const { GOOGLE_MAPS_API_KEY } = process.env;
+
+    const { currentRandomBusiness } = this.state;
+
+    let query;
+    if (Object.keys(currentRandomBusiness).length !== 0) {
+      query = currentRandomBusiness.alias + currentRandomBusiness.location.display_address;
+    }
+
+    return (
+      <Container>
+        <h1>Not sure where to eat? Have a random business chosen for you.</h1>
+        <RandomDropDown />
+        {
+          Object.keys(currentRandomBusiness).length > 1
+            ?
+            <RandomBusinessAndMapContainer>
+              <RandomBusinessContainer>
+                <RandomBusiness business={currentRandomBusiness} />
+              </RandomBusinessContainer>
+              <MapContainer>
+                <iframe
+                  frameBorder='0'
+                  style={{ border: '0', width: '100%', height: '100%' }}
+                  src={`https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAPS_API_KEY}&q=${query}`}>
+                </iframe>
+              </MapContainer>
+            </RandomBusinessAndMapContainer>
+            :
+            null
+        }
+      </Container>
+    );
+  }
 }
 
 Random.propTypes = {};
 
 const mapStateToProps = (state) => ({
   randomBusiness: state.random.randomBusiness,
+  currentRandomList: state.random.currentRandomList,
 });
 
 const mapDispatchToProps = {};
