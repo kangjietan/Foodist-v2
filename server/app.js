7582ae84da1169a8f682f8c9b7174cccdc8f842c
Kang Jie Tan <kangjietan@gmail.com>
2022-08-28 01:04:41 -0700

    Parse int for port

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT) || 3000;
 
 // Passport config
 require("../config/passport")(passport);
